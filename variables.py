06fa3c0cc58e31e01d98309486089d97c7f08b3c
Abraham Manda <abraham96manda@gmail.com>
2026-03-04 12:09:50 +0200

    Learn Variables

---
+++ variables.py
@@ -0,0 +1,7 @@
+failed_subjects = "2"
+name = "John"
+print ('Dear Mrs Badger')
+print ('Your son ' + name + ' is failing ' + failed_subjects + ' subjects at school')
+print ( name + ' will need to redo ' + failed_subjects + ' courses next year')
+name = "Eric"
+print ( name + ' is doing well in geography')
