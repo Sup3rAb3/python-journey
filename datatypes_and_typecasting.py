900a2d29e774d0505eaad03806dbe299c61bb394
Abraham Manda <abraham96manda@gmail.com>
2026-03-04 12:43:10 +0200

    datatypes and typecasting

---
+++ datatypes_and_typecasting.py
@@ -0,0 +1,24 @@
+failed_subjects = 2
+name = "John"
+is_going_to_party = False
+a = 'it\'s'
+print ('Dear Mrs Badger')
+print ('Your son ' + name + ' is failing ' + str(failed_subjects) + ' subjects at school')
+print ( name + ' will need to redo ' + str(failed_subjects) + ' courses next year')
+print ( 'Please ask ' + name + ' to learn python ' + a + ' preety cool')
+name = "Eric"
+print ( name + ' is doing well in geography')
+
+a = int(1)
+b = int(2.8)
+c = int("3")
+c1 = str(float("3.4"))
+d = float(1)
+e = float(2.8)
+f = float("3")
+g = float("4.23")
+h = str("80s")
+i = str(22)
+j = str(3.01) 
+
+print([a, b, c, c1, d, e, f, g, h, i, j])
