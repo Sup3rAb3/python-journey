a3e05d35c79c432259434619759062616bc75ff7
Abraham Manda <abraham96manda@gmail.com>
2026-03-05 11:33:38 +0200

    Add F string to receipt(arcade day)

--- arcade_day_challenge.py
+++ arcade_day_challenge.py
@@ -1,11 +1,11 @@
 print("Arcade Day Pass Tracker - Challenge Steps")
 
 customer_name = "Jacob"
-number_of_passes = 50
-tokens_per_pass = 2
-price_per_pass = 5.00
+number_of_passes = 5
+tokens_per_pass = 30
+price_per_pass = 12.50
 currency = "USD"
-tokens_required_per_game = 2
+tokens_required_per_game = 4
 
 #Caculations
 total_tokens = number_of_passes * tokens_per_pass
@@ -13,10 +13,10 @@ total_cost = number_of_passes * price_per_pass
 games_playable = total_tokens // tokens_required_per_game
 
 #Summary
-print("Summary" + "\nArcade Day Pass Tracker - Receipt")
+print("============ Summary ==========" + "\n========Arcade Day Pass Tracker - Receipt=============")
 print("Customer Name: " + customer_name)
 print("Number of Passes Purchased: " + str(number_of_passes))
 print("Tokens per Pass: " + str(tokens_per_pass))
-print("Total Tokens: " + str(total_tokens))
-print("Total Cost: " + str(total_cost) + " " + currency)
+print("Total Tokens:", total_tokens)
+print(f"Total Cost: ${total_cost:.2f} {currency}")
 print("Games Playable: " + str(games_playable))
