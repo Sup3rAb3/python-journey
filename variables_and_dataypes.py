299ae7610a6d533933ae9731f032f799f1693e36
Abraham Manda <abraham96manda@gmail.com>
2026-03-05 11:01:22 +0200

    Variables and Datatypes

---
+++ variables_and_dataypes.py
@@ -0,0 +1,8 @@
+print('Variables and Data Types - Exercise 1')
+# Create appropriate variables for Item Name, Item Price, and Item Quantity
+item_name = "Hammer"
+item_price = 9.99
+item_quantity = 5   
+item_currency = "USD"
+
+print('Item Name: ' + item_name, '\nItem Quantity: ' + str(item_quantity), '\nItem Price: ' + str(item_price) + ' ' + item_currency)
