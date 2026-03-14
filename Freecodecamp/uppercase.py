135ea8e752b3e45b81dfd3cb35869538e8d78666
Abraham Manda <abraham96manda@gmail.com>
2026-03-14 13:03:48 +0200

    Learn String Functions

--- Freecodecamp/uppercase.py
+++ Freecodecamp/uppercase.py
@@ -2,9 +2,54 @@ uppercase_my_str = "Hello World"
 uppercase_my_str = uppercase_my_str.upper()
 print(uppercase_my_str)
 
-
 lowercase_my_str = "Hello World"
 lowercase_my_str = lowercase_my_str.lower()
 print(lowercase_my_str)
 
+my_string = "FreeCode Camp"
+my_string = my_string.strip()
+print(my_string)
 
+my_string = "FreeCode Camp"
+replace_my_string = my_string.replace("FreeCode Camp", "FreeeJeff")
+print(replace_my_string)
+
+my_string = "Free Code Camp"
+split_my_string = my_string.split()
+print(split_my_string)
+
+my_list = ["What", "the", "heck?"]
+join_list = " ".join(my_list)
+print(join_list)
+
+my_string = "FreeCode Camp"
+starts_with_prefix = my_string.startswith("C")
+print(starts_with_prefix)
+
+my_string = "FreeCode Camp"
+ends_with_suffix = my_string.endswith("Camp")
+print(ends_with_suffix)
+
+my_string = "Cheese Cake and Muffins"
+find_substring = my_string.find("Cake")
+print(find_substring)
+
+my_string = "Cheese Cake and Muffins"
+count_substring = my_string.count("e")
+print(count_substring)
+
+my_string = "free code camp"
+capitalized_string = my_string.capitalize()
+print(capitalized_string)   
+
+my_string = "free code Camp"
+isuppercase = my_string.isupper()
+print(isuppercase)
+
+my_string = "FREE CODE CAMP"
+islowercase = my_string.islower()
+print(islowercase)
+
+my_string = "lil wayne and drake"
+title_string = my_string.title()
+print(title_string)
